473ae65be9f5213bf93f1bfd40fadcedf4730742
Documenter.jl <documenter@juliadocs.github.io>
2023-05-16 16:24:10 +0000

    build based on 675def1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RdJUFiles","category":"page"},{"location":"#RdJUFiles","page":"Home","title":"RdJUFiles","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RdJUFiles.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RdJUFiles]","category":"page"}]
+}

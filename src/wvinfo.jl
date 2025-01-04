8b37971da06a9f3b1af585a7877eebf432a8c5e7
nmaedajp <91188828+nmaedajp@users.noreply.github.com>
2025-01-04 15:00:08 +0900

    add oneline option to wvinfo.jl

--- src/wvinfo.jl
+++ src/wvinfo.jl
@@ -1,9 +1,14 @@
-function wvinfo(wave)
-    println("測定名 = ", wave.obs)
-    println("先頭時刻:　", wave.headtime)
-    println("波形サンプル数:　",wave.nwave)
-    println("サンプリング周波数:　",wave.hz)
-    println("チャンネル数：　",wave.nch)
-    println("チャンネルid: ", wave.chid)
-    println(" ")
+function wvinfo(wave; oneline = false)
+    if oneline 
+        println(wave.obs, " ", wave.headtime, " ", wave.nwave, " ", wave.hz,
+                " ", wave.nch, " ", wave.chid)
+    else
+        println(" ")
+        println("測定名 = ", wave.obs)
+        println("先頭時刻:　", wave.headtime)
+        println("波形サンプル数:　",wave.nwave)
+        println("サンプリング周波数:　",wave.hz)
+        println("チャンネル数：　",wave.nch)
+        println("チャンネルid: ", wave.chid)      
+    end  
 end
